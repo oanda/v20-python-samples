fc528e468a4d90aeb49b54be8627f0a4c3eb2866
Paulo Quinete <pquinete@pquinete-ud.oandacorp.com>
2019-03-21 11:16:57 -0400

    small fix on position/close.py

--- src/position/close.py
+++ src/position/close.py
@@ -57,8 +57,15 @@ def main():
     # v20 config file
     #
     api = args.config.create_context()
-
-    if args.long_units is not None:
+            
+    if args.long_units is not None and args.short_units is not None:
+        response = api.position.close(
+            account_id,
+            args.instrument,
+            longUnits=args.long_units,
+            shortUnits=args.short_units
+        )
+    elif args.long_units is not None:
         response = api.position.close(
             account_id,
             args.instrument,
@@ -68,13 +75,6 @@ def main():
         response = api.position.close(
             account_id,
             args.instrument,
-            shortUnits=args.short_units
-        )
-    else:
-        response = api.position.close(
-            account_id,
-            args.instrument,
-            longUnits=args.long_units,
             shortUnits=args.short_units
         )
 
